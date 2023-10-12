73b22bb243c1678e8850215d76a88918e8d7aa91
Brian Bauska <brianbauska@gmail.com>
2023-10-11 19:36:15 -0700

    Update global.js

--- js/global.js
+++ js/global.js
@@ -1,29 +1,28 @@
-/*
-Theme by: brian a. bauska (grandpa)
-*/
+/* global.js - theme by: brian a. bauska (grandpa) */
 $( function() {
-// Add background image
-		$.backstretch('images/Silus and Kitterson.jpg');
-//		$.backstretch('images/Silus Happy BDay from Bikini Bottom.jpg');
-// date of birth: 10/11/10.
-        var endDate = "Oct  11, 2023 23:59:59";
-        $('.countdown.simple').countdown({ date: endDate });
-        $('.countdown.styled').countdown({
-          date: endDate,
-          render: function(data) {
-            $(this.el).html("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
-          }
-        });
-        $('.countdown.callback').countdown({
-          date: +(new Date) + 10000,
-          render: function(data) {
-            $(this.el).text(this.leadingZeros(data.sec, 2) + " sec");
-          },
-          onEnd: function() {
+  // add background image
+  $.backstretch('images/Silus and Kitterson.jpg');
+  // date of birth: 10/11/10.
+  var endDate = "Oct  11, 2023 23:59:59";
+  $('.countdown.simple').countdown({ date: endDate });
+  $('.countdown.styled').countdown({
+    date: endDate,
+    render: function(data) {
+      $(this.el).html("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div><div>" 
+        + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" 
+        + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" 
+        + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+    }
+  });
+  $('.countdown.callback').countdown({
+    date: +(new Date) + 10000,
+    render: function(data) {
+      $(this.el).text(this.leadingZeros(data.sec, 2) + " sec");
+    },
+    onEnd: function() {
             $(this.el).addClass('ended');
-          }
-        }).on("click", function() {
-          $(this).removeClass('ended').data('countdown').update(+(new Date) + 10000).start();
-        });
-      });
-   
+    }
+  }).on("click", function() {
+    $(this).removeClass('ended').data('countdown').update(+(new Date) + 10000).start();
+  });
+});
